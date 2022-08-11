368ec5716d357d34f658f7a10efb175ae33771f1
kiratae <kirataetwo@gmail.com>
2022-08-11 17:39:35 +0700

    update - itas theme

--- dist/js/we-custom.js
+++ dist/js/we-custom.js
@@ -1,17 +1,8 @@
-/**
- * AdminLTE Demo Menu
- * ------------------
- * You should not use this file in production.
- * This file is for demo purposes only.
- */
-
 /* eslint-disable camelcase */
 
 (function () {
   'use strict'
-  $('.sidebar-mini .nav-sidebar[data-widget="treeview"] .nav-item').off('click').on('click', function (e) {
-    e.preventDefault()
-    console.log('test')
-    return false
+  $('.sidebar-mini .nav-sidebar[data-widget="treeview"] .nav-item').on('click', function () {
+    return $('.sidebar-mini.sidebar-collapse').length !== 1
   })
 })(jQuery)
