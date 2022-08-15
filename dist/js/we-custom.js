9d357818dc3b9ad1d5db54dd77650e7c48234a81
kiratae <kirataetwo@gmail.com>
2022-08-15 18:02:04 +0700

    update ita theme

--- dist/js/we-custom.js
+++ dist/js/we-custom.js
@@ -2,7 +2,11 @@
 
 (function () {
   'use strict'
-  $('.sidebar-mini .nav-sidebar[data-widget="treeview"] .nav-item').on('click', function () {
+  $('.sidebar-mini.sidebar-collapse .nav-sidebar[data-widget="treeview"] > .nav-item > a').on('click', function (e) {
+    if (e.target !== this) {
+      return
+    }
+
     return $('.sidebar-mini.sidebar-collapse').length !== 1
   })
 })(jQuery)
